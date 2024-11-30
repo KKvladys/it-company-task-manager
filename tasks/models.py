b055b94d029ff8f354f2e34ac7a9907916443b34
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-11-30 13:06:56 +0200

    add models Position Task TaskType

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,32 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+class Position(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("Urgent", "Urgent"),
+        ("High", "High"),
+        ("Medium", "Medium"),
+        ("Low", "Low"),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(TaskType, related_name="tasks", on_delete=models.SET_NULL)
+    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks")

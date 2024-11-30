a539970c2c87e89ea4219016c17a1d1716d4377e
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-11-30 13:50:53 +0200

    add migrations

--- tasks/models.py
+++ tasks/models.py
@@ -28,5 +28,11 @@ class Task(models.Model):
     deadline = models.DateTimeField()
     is_completed = models.BooleanField(default=False)
     priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES)
-    task_type = models.ForeignKey(TaskType, related_name="tasks", on_delete=models.SET_NULL)
+    task_type = models.ForeignKey(
+        TaskType,
+        related_name="tasks",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
     assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks")

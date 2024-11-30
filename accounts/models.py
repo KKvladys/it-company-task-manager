75d3c3a0d811891abce770273b9945c03e038f45
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-11-30 13:27:40 +0200

    create model Worker

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from tasks.models import Position
+
+
+class Worker(AbstractUser):
+    email = models.EmailField(unique=True)
+    position = models.ForeignKey(
+        Position,
+        related_name="workers",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"

702307553d823dbdbc48cc59d444fe6d68c294e5
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-12-02 15:15:52 +0200

    add app name in tasks url

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
 from tasks.views import (
-    index,
+    home,
     TaskListView,
     TaskDtailView,
     TaskHistoryListView
 )
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", home, name="home"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/history/", TaskHistoryListView.as_view(), name="task-list-history"),
     path("<int:pk>/tasks/", TaskDtailView.as_view(), name="task-detail")

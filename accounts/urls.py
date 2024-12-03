4836478f03512eb959bc55cf4b51af7b89468ef6
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-12-03 17:48:27 +0200

    add form to model Worker

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,16 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 
-from accounts.views import WorkerListView, WorkerDetailView, WorkerUpdateView, WorkerDeleteView
+from accounts.views import WorkerListView, WorkerCreateView, WorkerDetailView, WorkerUpdateView, WorkerDeleteView
 
 app_name = "accounts"
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("login/", LogoutView.as_view(next_page='/login/'), name="login"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
     path("<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
-    path("<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete")
-    # path("register/", WorkerCreateView.as_view(), name="register")
+    path("<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete"),
+    path("register/", WorkerCreateView.as_view(), name="register")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,17 @@
-from django.contrib.auth import get_user_model
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, authenticate, login
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views import generic
 
-from accounts.forms import RegisterForm
+from accounts.forms import RegisterForm, LoginForm, WorkerUpdateForm
 from accounts.models import Worker
 
+User = get_user_model()
+class WorkerCreateView(generic.CreateView):
+    model = Worker
+    form_class = RegisterForm
+    template_name = "registration/register.html"
 
-# class WorkerCreateView(generic.CreateView):
-#     model = Worker
-#     form_class = RegisterForm
-#     template_name = "registration/register.html"
-#
 
 class WorkerListView(generic.ListView):
     model = get_user_model()
@@ -22,7 +23,55 @@ class WorkerDetailView(generic.DetailView):
 
 class WorkerUpdateView(generic.UpdateView):
     model = get_user_model()
+    form_class = WorkerUpdateForm
+    success_url = reverse_lazy("accounts:worker-list")
 
 
 class WorkerDeleteView(generic.DetailView):
     model = get_user_model()
+
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+
+    msg = None
+
+    if request.method == "POST":
+
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                msg = 'Invalid credentials'
+        else:
+            msg = 'Error validating the form'
+
+    return render(request, "accounts/login.html", {"form": form, "msg": msg})
+
+def register_user(request):
+    msg = None
+    success = False
+
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+
+            msg = 'User created - please <a href="/login">login</a>.'
+            success = True
+
+            return redirect("accounts:login/")
+
+        else:
+            msg = 'Form is not valid'
+    else:
+        form = RegisterForm()
+
+    return render(request, "registration/register.html", {"form": form, "msg": msg, "success": success})

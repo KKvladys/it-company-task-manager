4e848d5c73c51def08e04d9a0afd598af3745f95
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-12-12 16:47:05 +0200

    change priority choices in model task

--- tasks/models.py
+++ tasks/models.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.db import models
 
 
-
 class TaskType(models.Model):
     name = models.CharField(max_length=63)
 
     def __str__(self):
         return self.name
+
 
 class Position(models.Model):
     name = models.CharField(max_length=63)
@@ -15,19 +15,20 @@ class Position(models.Model):
     def __str__(self):
         return self.name
 
+
 class Task(models.Model):
     PRIORITY_CHOICES = [
-        ("Urgent", "Urgent"),
-        ("High", "High"),
-        ("Medium", "Medium"),
-        ("Low", "Low"),
+        (0, "Urgent"),
+        (1, "High"),
+        (2, "Medium"),
+        (3, "Low"),
     ]
 
     name = models.CharField(max_length=255)
     description = models.TextField()
     deadline = models.DateTimeField()
     is_completed = models.BooleanField(default=False)
-    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES)
+    priority = models.IntegerField(choices=PRIORITY_CHOICES)
     task_type = models.ForeignKey(
         TaskType,
         related_name="tasks",
@@ -36,7 +37,6 @@ class Task(models.Model):
         blank=True
     )
     assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks")
-
 
     def __str__(self):
         return self.name
